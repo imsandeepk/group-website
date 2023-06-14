24fde0c37d100055fa0dba18a4210fe79e52cd0f
Sandeep Kumar <65333883+imsandeepk@users.noreply.github.com>
2023-06-14 01:51:18 +0100

    CV added

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Header from "./component/Header";
 import Team from "./component/Team";
 import Info from "./component/Info";
 import About from './component/About';
+import CV from './component/CV';
 
 function App() {
   return (
@@ -24,6 +25,7 @@ function App() {
           <Route path="/About" element={<About/>
           } />
           <Route path="/team" element={<Team />} />
+          <Route path="/CV" element={<CV />} />
           <Route path="/contact" element={<Contact />} />
         </Routes>
 

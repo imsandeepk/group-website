7918c4e5a1cc0a341f0a7c9354c0567ea79fd2e8
Sandeep Kumar <65333883+imsandeepk@users.noreply.github.com>
2023-06-14 02:27:26 +0100

    Update Header.js

--- src/component/Header.js
+++ src/component/Header.js
@@ -3,33 +3,45 @@ import { Link } from 'react-router-dom';
 
 export default function Header() {
   return (
-    <nav className="navbar bg-dark navbar-expand-lg" style={{ height: '25vh' }}>
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark" style={{ height: '25vh' }}>
       <div className="container-fluid h-100 d-flex flex-column justify-content-end">
         <h1 className="text-center" style={{ color: '#FFD800' }}>
           PROFESSOR LINJIANG CHEN
         </h1>
-        <div className="navbar-nav justify-content-center text-white">
-          <Link className="nav-link mx-3 text-white" to="/">
-            Home
-          </Link>
-          <Link className="nav-link mx-4 text-white" to="/about">
-            Linjiang Chen
-          </Link>
-          <a className="nav-link mx-4 text-white" href="https://scholar.google.co.uk/citations?user=ZprWaMkAAAAJ&hl=en">
-            Publications
-          </a>
-          <Link className="nav-link mx-4 text-white" to="/CV">
-            CV
-          </Link>
-          <Link className="nav-link mx-4 text-white" to="/team">
-            Group
-          </Link>
-          <Link className="nav-link mx-4 text-white" to="/contact">
-            Contact US
-          </Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse justify-content-center" id="navbarNav">
+          <div className="navbar-nav">
+            <Link className="nav-link mx-3 text-white" to="/">
+              Home
+            </Link>
+            <Link className="nav-link mx-4 text-white" to="/about">
+              Linjiang Chen
+            </Link>
+            <a className="nav-link mx-4 text-white" href="https://scholar.google.co.uk/citations?user=ZprWaMkAAAAJ&hl=en">
+              Publications
+            </a>
+            <Link className="nav-link mx-4 text-white d-none d-lg-inline" to="/CV">
+              CV
+            </Link>
+            <Link className="nav-link mx-4 text-white d-none d-lg-inline" to="/team">
+              Group
+            </Link>
+            <Link className="nav-link mx-4 text-white" to="/contact">
+              Contact US
+            </Link>
+          </div>
         </div>
       </div>
     </nav>
   );
 }
-

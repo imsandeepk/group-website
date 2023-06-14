eeece238ba87055c3a4f01710550b9637ab0ef49
Sandeep Kumar <65333883+imsandeepk@users.noreply.github.com>
2023-06-14 02:22:27 +0100

    Update Team.js

--- src/component/Team.js
+++ src/component/Team.js
@@ -1,7 +1,97 @@
-import React from 'react'
+import React from 'react';
 
 export default function Team() {
   return (
-    <div>Team</div>
-  )
+    <div style={{ backgroundColor: "#e3f2fd"}}>
+        <div className='container'>
+        <h1 className='text-center'>Group Members</h1>
+      <div className='row'>
+      <h3 className='text-center py-3 text-decoration-underline'>Postdoc Researchers</h3>
+        <div className='col-md-4'>
+          <div className='card' style={{ width: '12rem', }}>
+            <img src='cat.png' className='card-img-top' alt='...' />
+            <div className='card-body'>
+              <h5 className='card-title text-center'>Name</h5>
+              <div className='text-center'>
+                  <a href='#' className='btn btn-primary'>
+                    Website/Gmail
+                  </a>
+                </div>
+            </div>
+          </div>
+        </div>
+        </div>
+      <div className='row'>
+      <h3 className='text-center py-3 text-decoration-underline'>PHD students</h3>
+        <div className='col-md-4'>
+          <div className='card' style={{ width: '12rem', }}>
+            <img src='cat.png' className='card-img-top' alt='...' />
+            <div className='card-body'>
+              <h5 className='card-title text-center'>Name</h5>
+              <div className='text-center'>
+                  <a href='#' className='btn btn-primary'>
+                    Website/Gmail
+                  </a>
+                </div>
+            </div>
+          </div>
+        </div>
+        </div>
+
+      <div className='row'>
+      <h3 className='text-center py-3 text-decoration-underline'>Graduate Students</h3>
+        <div className='col-md-4'>
+          <div className='card' style={{ width: '12rem', }}>
+            <img src='cat.png' className='card-img-top' alt='...' />
+            <div className='card-body'>
+              <h5 className='card-title text-center'>Name</h5>
+              <div className='text-center'>
+                  <a href='#' className='btn btn-primary'>
+                    Website/Gmail
+                  </a>
+                </div>
+            </div>
+          </div>
+        </div>
+        </div>
+
+      <div className='row'>
+      <h3 className='text-center py-3 text-decoration-underline'>Undergraduate Students</h3>
+        <div className='col-md-4'>
+          <div className='card' style={{ width: '12rem', }}>
+            <img src='cat.png' className='card-img-top' alt='...' />
+            <div className='card-body'>
+              <h5 className='card-title text-center'>Name</h5>
+              <div className='text-center'>
+                  <a href='#' className='btn btn-primary'>
+                    Website/Gmail
+                  </a>
+                </div>
+            </div>
+          </div>
+        </div>
+        </div>
+
+        
+      <div className='row'>
+      <h3 className='text-center py-3 text-decoration-underline'>Alumni </h3>
+        <div className='col-md-4'>
+          <div className='card' style={{ width: '12rem', }}>
+            <img src='cat.png' className='card-img-top' alt='...' />
+            <div className='card-body'>
+              <h5 className='card-title text-center'>Name</h5>
+              <div className='text-center'>
+                  <a href='#' className='btn btn-primary'>
+                    Website/Gmail
+                  </a>
+                </div>
+            </div>
+          </div>
+        </div>
+        </div>
+
+        </div>
+
+    </div>
+  );
 }
